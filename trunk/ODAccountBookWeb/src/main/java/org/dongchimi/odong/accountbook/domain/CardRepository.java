34b677a5e5c323dfeaa6528424b0205b8d7ca9f3
duckierS2@gmail.com <duckierS2@gmail.com@d26c91d3-b69e-f62f-f19c-a33abaa06132>
2015-06-26 07:00:39 +0000

    카드관리 관련 클래스 생성

---
+++ trunk/ODAccountBookWeb/src/main/java/org/dongchimi/odong/accountbook/domain/CardRepository.java
@@ -0,0 +1,7 @@
+package org.dongchimi.odong.accountbook.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CardRepository extends JpaRepository<Card, Long> {
+
+}
